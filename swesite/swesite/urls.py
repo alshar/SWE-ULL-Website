0552c3cf97c2d43d7805d3dabca8c8ca92dc0234
Al Sharairi <asharairi@gmail.com>
2019-05-22 18:26:09 -0500

    add urls.py to base app

--- swesite/swesite/urls.py
+++ swesite/swesite/urls.py
@@ -24,6 +24,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pointstracker', include('pointstracker.urls')),
+    path('', include('base.views')),
 
 
 
