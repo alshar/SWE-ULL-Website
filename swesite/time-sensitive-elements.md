106a3b529e58b8aedc2751d88ea958ff47a2453e
Al Sharairi <asharairi@gmail.com>
2019-06-08 19:55:27 -0500

    add volunteer points app

--- swesite/time-sensitive-elements.md
+++ swesite/time-sensitive-elements.md
@@ -1,3 +1,4 @@
 ###Time Sensitive Elements On This Website
 
 
+[comment]: TODO
---
+++ swesite/volunteerpoints/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class VolunteerpointsConfig(AppConfig):
+    name = 'volunteerpoints'
