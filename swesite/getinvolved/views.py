cd826b9d230b8efe2b8a674f4f18ed2dc2638f8a
Al Sharairi <asharairi@gmail.com>
2019-06-16 17:02:29 -0500

    move get involved template to project level template dir

--- swesite/getinvolved/views.py
+++ swesite/getinvolved/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.template.response import TemplateResponse
 
 def getinvolved(request):
-    return render(request, 'getinvolved/getinvolved.html')
+    return TemplateResponse(request, 'getinvolved/getinvolved.html')
