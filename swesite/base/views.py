8d648c634c7e1906bddd645f8fd07b375ace73b9
Al Sharairi <asharairi@gmail.com>
2019-07-04 22:00:34 -0500

    add: context for points needed

--- swesite/base/views.py
+++ swesite/base/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import TemplateView
 from swesite.contexts.swe_social_context import swe_social
+from swesite.contexts.swe_volunteer_context import swe_volunteer
 
 
 class baseindex(TemplateView):
@@ -8,5 +9,6 @@ class baseindex(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(baseindex, self).get_context_data(**kwargs)
         context['swe_social'] = swe_social(request=baseindex)
+        context['swe_volunteer'] = swe_volunteer(request=baseindex)
         return context
 
---
+++ swesite/swesite/contexts/swe_volunteer_context.py
@@ -0,0 +1,5 @@
+def swe_volunteer(request):
+    context = {
+        'points_needed': '30'
+    }
+    return context
