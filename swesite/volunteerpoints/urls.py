0e38b8c8abe37ed1a09d9dfcfef82366f08676a7
Al Sharairi <asharairi@gmail.com>
2019-07-04 22:20:40 -0500

    add: context to time sense elems - volunteer points

--- swesite/volunteerpoints/urls.py
+++ swesite/volunteerpoints/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.volunteerpoints, name='volunteerpoints'),
+    path('', views.volunteerpoints.as_view(), name='volunteerpoints'),
 ]
--- swesite/volunteerpoints/views.py
+++ swesite/volunteerpoints/views.py
@@ -1,5 +1,11 @@
-from django.template.response import TemplateResponse
+from django.views.generic import TemplateView
+from swesite.contexts.swe_volunteer_context import swe_volunteer
 
 
-def volunteerpoints(request):
-    return TemplateResponse(request, 'volunteerpoints/volunteerpoints.html')
+class volunteerpoints(TemplateView):
+    template_name = 'volunteerpoints/volunteerpoints.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(volunteerpoints, self).get_context_data(**kwargs)
+        context['swe_volunteer'] = swe_volunteer(request=volunteerpoints)
+        return context
