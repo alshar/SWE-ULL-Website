e768b4e96ae995f96c2fc99dc7b7a198b645b581
Al Sharairi <asharairi@gmail.com>
2019-07-04 22:13:23 -0500

    add: context to conference link - homepage

--- swesite/swesite/contexts/swe_volunteer_context.py
+++ swesite/swesite/contexts/swe_volunteer_context.py
@@ -1,5 +1,7 @@
 def swe_volunteer(request):
     context = {
+        'conference_abbreviation': 'WE19',
+        'conference_website': 'https://we19.swe.org/',
         'points_needed': '30'
     }
     return context
