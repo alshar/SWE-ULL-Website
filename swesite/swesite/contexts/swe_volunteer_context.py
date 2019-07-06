902dae4200e60fc56f078d67643cf35764e9bb36
Al Sharairi <asharairi@gmail.com>
2019-07-06 18:06:44 -0500

    add: context vars to volunteer points page

--- swesite/swesite/contexts/swe_volunteer_context.py
+++ swesite/swesite/contexts/swe_volunteer_context.py
@@ -2,6 +2,7 @@ def swe_volunteer(request):
     context = {
         'conference_abbreviation': 'WE19',
         'conference_website': 'https://we19.swe.org/',
-        'points_needed': '30'
+        'points_needed': '30',
+        'students_taking':'20'
     }
     return context
--- swesite/volunteerpoints/views.py
+++ swesite/volunteerpoints/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import TemplateView
+from swesite.contexts.swe_social_context import swe_social
 from swesite.contexts.swe_volunteer_context import swe_volunteer
+from swesite.contexts.swe_officers_context import swe_officers_secondary
 
 
 class volunteerpoints(TemplateView):
@@ -8,4 +10,6 @@ class volunteerpoints(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(volunteerpoints, self).get_context_data(**kwargs)
         context['swe_volunteer'] = swe_volunteer(request=volunteerpoints)
+        context['swe_social'] = swe_social(request=volunteerpoints)
+        context['swe_officers_secondary'] = swe_officers_secondary(request=volunteerpoints)
         return context
