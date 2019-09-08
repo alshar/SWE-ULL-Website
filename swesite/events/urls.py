3afc4743a4cdae923a18a8c20f9759f4a54db433
Al Sharairi <asharairi@gmail.com>
2019-09-07 21:47:49 -0500

    add: spring 19 gallery template / url / view

--- swesite/events/urls.py
+++ swesite/events/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from events import views
 
 urlpatterns = [
-    path('', views.events.as_view(), name='events'),
     path('fall18/', views.fall18.as_view(), name='fall18'),
-    path('spring18', views.spring18.as_view(), name='spring18')
+    path('spring18/', views.spring18.as_view(), name='spring18'),
+    path('spring19/', views.spring19.as_view(), name='spring19'),
+
+    path('', views.events.as_view(), name='events'),
+
 ]
--- swesite/events/views.py
+++ swesite/events/views.py
@@ -5,12 +5,9 @@ from swesite.contexts.swe_social_context import swe_social
 class events(TemplateView):
     template_name = 'events/events.html'
 
-    def get_context_data(self, **kwargs):
-        context = super(events, self).get_context_data(**kwargs)
-        context['swe_social'] = swe_social(request=events)
-        return context
 
-
+class spring19(TemplateView):
+    template_name = 'events/spring_19_gallery.html'
 
 class fall18(TemplateView):
     template_name = 'events/fall18gallery.html'
