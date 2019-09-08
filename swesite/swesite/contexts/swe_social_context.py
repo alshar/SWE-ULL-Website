17847386c4452e3740718ed12fe03c0a39ecced9
Al Sharairi <asharairi@gmail.com>
2019-09-07 21:28:29 -0500

    remove org-sync
    will no longer be used

--- swesite/swesite/contexts/swe_social_context.py
+++ swesite/swesite/contexts/swe_social_context.py
@@ -3,6 +3,5 @@ def swe_social(request):
         'email': 'swe.ulchapter@gmail.com',
         'facebook': 'https://www.facebook.com/groups/774749222697769/about/',
         'youtube': 'https://www.youtube.com/channel/UCW_rAv6ZARuX8f7bAq2LjLA/featured?view_as=subscriber.com/',
-        'orgsync': 'https://orgsync.com/124699/chapter'
     }
     return context
