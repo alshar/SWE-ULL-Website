556c91a30b259ed3f8ae510aeb710ffbf2e6f102
Al Sharairi <asharairi@gmail.com>
2019-09-07 22:33:01 -0500

    add: fall 19 gallery context to current events

--- swesite/events/views.py
+++ swesite/events/views.py
@@ -1,10 +1,14 @@
 from django.views.generic import TemplateView
-from swesite.contexts.swe_social_context import swe_social
+from swesite.contexts.swe_events_context import fall_19
 
 
 class events(TemplateView):
     template_name = 'events/events.html'
 
+    def get_context_data(self, **kwargs):
+        context = super(events, self).get_context_data(**kwargs)
+        context['fall_19'] = fall_19(request=events)
+        return context
 
 class spring19(TemplateView):
     template_name = 'events/spring_19_gallery.html'
---
+++ swesite/swesite/contexts/swe_events_context.py
@@ -0,0 +1,22 @@
+def fall_19(request):
+    context = {
+        'meeting_1': {
+            'display_title': 'First General Meeting',
+            'display_date': '9/11',
+            'img_paths': {
+                'path_1': 'events/static/events/img/carousel/fall19/meeting_1/meeting_1_1.png'
+            }
+        },
+
+        'les_bbq': {
+            'display_title': 'Louisiana Engineering Society BBQ',
+            'display_date': '9/6',
+            'img_paths': {
+                'path_1': 'events/static/events/img/carousel/fall19/les_bbq/les_bbq_1.JPG',
+                'path_2': 'events/static/events/img/carousel/fall19/les_bbq/les_bbq_2.JPG'
+            }
+        }
+
+    }
+
+    return context
