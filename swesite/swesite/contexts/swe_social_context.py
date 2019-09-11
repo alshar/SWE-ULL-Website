5965ea0131fa823ee2650b3e58f609432391de3c
Al Sharairi <asharairi@gmail.com>
2019-09-10 21:17:02 -0500

    add: instagram button to footer

--- swesite/swesite/contexts/swe_social_context.py
+++ swesite/swesite/contexts/swe_social_context.py
@@ -3,5 +3,6 @@ def swe_social(request):
         'email': 'swe.ulchapter@gmail.com',
         'facebook': 'https://www.facebook.com/groups/774749222697769/about/',
         'youtube': 'https://www.youtube.com/channel/UCW_rAv6ZARuX8f7bAq2LjLA/featured?view_as=subscriber.com/',
+        'instagram': 'https://www.instagram.com/swe_ulchapter/?hl=en'
     }
     return context
