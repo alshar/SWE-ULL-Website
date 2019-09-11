4f346618c69628be503819fb60faebd11201ce72
Al Sharairi <asharairi@gmail.com>
2019-09-10 21:29:32 -0500

    update: move upcoming events to its own block
    I kept the event dict commented since I'm gonna need it soon anyway

--- swesite/swesite/contexts/swe_events_context.py
+++ swesite/swesite/contexts/swe_events_context.py
@@ -1,20 +1,28 @@
 def fall_19(request):
     context = {
-        'bake_sale_1': {
-            'display_title': 'Bake Sale!',
-            'display_date': '9/20',
+        'upcoming_events': {
+            'display_title': 'Upcoming Events',
+            'display_date': 'See Flyer for Dates',
             'img_paths': [
-                'events/img/carousel/fall19/bake_sale_1/flyer.png'
+                'events/img/carousel/fall19/bake_sale_1/flyer.png',
+                'events/img/carousel/fall19/meeting_1/meeting_1_1.png'
             ]
         },
-
-        'meeting_1': {
-            'display_title': 'First General Meeting',
-            'display_date': '9/11',
-            'img_paths': [
-                'events/img/carousel/fall19/meeting_1/meeting_1_1.png',
-            ]
-        },
+        # 'bake_sale_1': {
+        #     'display_title': 'Bake Sale!',
+        #     'display_date': '9/20',
+        #     'img_paths': [
+        #         'events/img/carousel/fall19/bake_sale_1/flyer.png'
+        #     ]
+        # },
+        #
+        # 'meeting_1': {
+        #     'display_title': 'First General Meeting',
+        #     'display_date': '9/11',
+        #     'img_paths': [
+        #         'events/img/carousel/fall19/meeting_1/meeting_1_1.png',
+        #     ]
+        # },
 
         'les_bbq': {
             'display_title': 'Louisiana Engineering Society BBQ',
