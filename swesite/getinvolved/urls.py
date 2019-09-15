82804faff1c218b81d9cc497f9e443c4bb216be5
Al Sharairi <asharairi@gmail.com>
2019-09-14 22:17:53 -0500

    fix: swe_social context to every page
    I should write some middleware to this for me. Every page needs to have the swe_social context since every page has a footer

--- swesite/getinvolved/urls.py
+++ swesite/getinvolved/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.getinvolved, name='getinvolved'),
+    path('', views.getinvolved.as_view(), name='getinvolved'),
 ]
--- swesite/pointtracker/views.py
+++ swesite/pointtracker/views.py
@@ -1,9 +1,12 @@
-from django.contrib.auth.views import LoginView
 from django.views.generic import TemplateView
 
-# from pointtracker.forms import SWEUserLogin
+from swesite.contexts.swe_social_context import swe_social
 
 
 class PointTrackerLogin(TemplateView):
     template_name = 'pointtracker/pointtracker.html'
-    # authentication_form = SWEUserLogin
+
+    def get_context_data(self, **kwargs):
+        context = super(PointTrackerLogin, self).get_context_data(**kwargs)
+        context['swe_social'] = swe_social(request=PointTrackerLogin)
+        return context
