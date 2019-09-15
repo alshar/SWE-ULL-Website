62e084091c8a16d717874b15e34de80f05a4cff6
Al Sharairi <asharairi@gmail.com>
2019-09-15 14:31:40 -0500

    add: login form to point tracker

--- swesite/pointtracker/forms.py
+++ swesite/pointtracker/forms.py
@@ -1,5 +1,6 @@
+from crispy_forms.bootstrap import PrependedText
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit, Layout, ButtonHolder, Fieldset, Div, Row, Column, HTML
+from crispy_forms.layout import Submit, Layout, ButtonHolder, Fieldset, Div, Row, Column, HTML, Field
 from django import forms
 from django.urls import reverse
 
@@ -27,13 +28,14 @@ class PointTrackerLoginForm(forms.Form):
         super(PointTrackerLoginForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper(self)
         self.helper.form_method = 'post'
-        self.helper.form_class = 'card justify-content-center text-center'
+        self.helper.form_class = 'justify-content-center text-center'
+        self.helper.field_class = ''
         self.helper.form_action = reverse('pointtracker')
 
         self.helper.layout = Layout(
-            'ulid',
-            'first_name',
-            'last_name',
+            Field('first_name'),
+            Field('last_name'),
+            Field('ulid'),
 
-            Submit('submit', 'Submit')
+            Submit('submit', 'Submit', css_class='btn btn-primary mt-4')
         )
