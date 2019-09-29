a6efcf55b79d85dc88680785940b4613382aa1ed
Al Sharairi <asharairi@gmail.com>
2019-09-29 16:56:37 -0500

    add: donut sits

--- swesite/swesite/contexts/swe_events_context.py
+++ swesite/swesite/contexts/swe_events_context.py
@@ -1,12 +1,13 @@
 def fall_19(request):
     context = {
-        # 'bake_sale_1': {
-        #     'display_title': 'Upcoming Events',
-        #     'display_date': 'See Flyer for Dates',
-        #     'img_paths': [
-        #         'events/img/carousel/fall19/bake_sale_1/flyer.png',
-        #     ]
-        # },
+        'upcoming_events': {
+            'display_title': 'Upcoming Events',
+            'display_date': 'See Flyer for Dates',
+            'img_paths': [
+                'events/img/carousel/fall19/donuts_sits/donuts_sits_flyer.jpg',
+            ]
+        },
+
         'bake_sale_1': {
             'display_title': 'Bake Sale',
             'display_date': '9/20/19',
@@ -26,7 +27,6 @@ def fall_19(request):
                 'events/img/carousel/fall19/meeting_1/m_2.JPG',
                 'events/img/carousel/fall19/meeting_1/m_3.JPG',
                 'events/img/carousel/fall19/meeting_1/m_4.JPG',
-
 
             ]
         },
