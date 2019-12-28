a567ff067bacc875a2fe5fb873862a68b28c7866
Al Sharairi <asharairi@gmail.com>
2019-12-27 19:33:22 -0600

    add: WE20 info

--- swesite/swesite/contexts/swe_volunteer_context.py
+++ swesite/swesite/contexts/swe_volunteer_context.py
@@ -1,8 +1,8 @@
 def swe_volunteer(request):
     context = {
-        'conference_abbreviation': 'WE19',
-        'conference_website': 'https://we19.swe.org/',
-        'points_needed': '30',
-        'students_taking':'20'
+        'conference_abbreviation': 'WE20',
+        'conference_website': 'https://partnerguide.swe.org/conferences/',
+        'points_needed': '20',
+        'students_taking': '30'
     }
     return context
